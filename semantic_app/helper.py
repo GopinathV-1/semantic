fd4255cec1bb30853d882e979c8583de6b5a9f69
Gopinath <gopinath@instamojo.com>
2022-07-30 20:09:21 +0530

    Add create_sales_entries

--- semantic_app/helper.py
+++ semantic_app/helper.py
@@ -10,3 +10,10 @@ def fetch_sales(year, drug_type):
         format_value.append(i.__dict__)
     
     return format_value
+
+
+def create_sales_entries():
+    with open('PharmaSales.csv', 'r') as file:
+        drug = csv.DictReader(file)
+        for row in drug:
+            DrugSales.objects.create(name='', drug_type='', day='', month='', year='', sales='')
