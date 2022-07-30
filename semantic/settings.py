0873511d6a671b3583d75222842934586d73595e
Gopinath <gopinath@instamojo.com>
2022-07-30 20:22:31 +0530

    Removed django secret_key

--- semantic/settings.py
+++ semantic/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dxn5t)u&=5aldrg($+od)cpf65yny!3&=5bqdg1q%7a3oof4uk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
