7e66ca41871360a751424146383c9813687eb3aa
Gopinath <gopinath@instamojo.com>
2022-07-30 20:35:26 +0530

    Update Create_sales_entries

--- semantic_app/helper.py
+++ semantic_app/helper.py
@@ -12,8 +12,21 @@ def fetch_sales(year, drug_type):
     return format_value
 
 
+id,datum,m01ab,m01ae,n02ba,n02be,n05b,n05c,r03,r06,year
+drugs = ['m01ab','m01ae','n02ba','n02be','n05b','n05c','r03','r06'] 
 def create_sales_entries():
     with open('PharmaSales.csv', 'r') as file:
         drug = csv.DictReader(file)
         for row in drug:
-            DrugSales.objects.create(name='', drug_type='', day='', month='', year='', sales='')
+            for i in range(6):
+                drug_type = ''
+                if i < 2:
+                    drug_type =  'M'
+                elif i < 5:
+                    drug_type = 'N'
+                else:
+                    drug_type = 'R'
+                
+                day = str(row[datum])[8:]
+                month = str(row[datum])[5:7]
+                DrugSales.objects.create(name=drugs[i], drug_type=drug_type, day=int(day), month=int(month), year=row['year'], sales=row[drugs[i]])
