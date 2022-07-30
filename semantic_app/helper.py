c57aaefa3d5a472bc118415f99dd3e65c5aab5bd
Gopinath <gopinath@instamojo.com>
2022-07-30 19:49:13 +0530

    Updated method fetch_sales

--- semantic_app/helper.py
+++ semantic_app/helper.py
@@ -1,18 +1,12 @@
-from models import DrugSales,DrugReview
-
-
-def querySingle(sql):
-    try:
-        cursor = cnx.cursor()
-        cursor.execute(sql)
-        for row in cursor:
-            return dict((column[0],row[index]) for index, column in enumerate(cursor.description))
-        return {}
-    finally:
-        cursor.close()
+from models import DrugSales, DrugReview
 
 
 def fetch_sales(year, drug_type):
 
     sales = DrugSales.objects.get(year=year, drug_type=drug_type)
     output = sales.objects.values('name').order_by('name').annotate(total_sales=sum('sales'))
+    format_value = []
+    for i in output:
+        format_value.append(i.__dict__)
+    
+    return format_value
